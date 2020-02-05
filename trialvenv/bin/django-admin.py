0d2a7f0bad46b0af8d6f5f031f27206053bf5e55
Wanjiru Catherine <catherinemunene802@gmail.com>
2020-02-05 10:33:12 +0300

    Started the Inventory System

---
+++ trialvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/shiru/PersonalProjects/DjangoProjects/TrialApp/trialvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
